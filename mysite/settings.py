8921ba6c3186d1ca3f641d5732e9c10934bd06b4
Rahul20s <156184987+Rahul20s@users.noreply.github.com>
2026-04-07 08:07:39 +0000

    Add production deployment files for Render

--- mysite/settings.py
+++ mysite/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-deo4fr@)o*9u)y42q1e-s%jo93s8v*l$x7lt+td8un8r*0sx+#'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-deo4fr@)o*9u)y42q1e-s%jo93s8v*l$x7lt+td8un8r*0sx+#')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',') if os.environ.get('ALLOWED_HOSTS') else []
 
 
 # Application definition
@@ -119,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 LOGIN_URL = 'login'
 
